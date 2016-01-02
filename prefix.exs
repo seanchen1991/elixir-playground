1d9d022919954c7975533fcd70bafecede4431f7
Sean Chen <skypemaster007@gmail.com>
2016-01-02 15:26:24 -0800

    Add a bunch of exercises

---
+++ prefix.exs
@@ -0,0 +1,9 @@
+prefix = fn a ->
+    fn b ->
+        "#{a} #{b}"
+    end
+end
+
+mrs = prefix.("Mrs")
+IO.puts mrs.("Smith")
+IO.puts prefix.("Elixir").("Rocks")
---
+++ recursion.exs
@@ -0,0 +1,14 @@
+defmodule Factorial do
+    def of(0), do: 1
+    def of(n), do: n * of(n - 1)
+end
+
+defmodule Sum do
+    def of(0), do: 0
+    def of(n), do: n + of(n - 1)
+end
+
+defmodule Gcd do
+    def of(x, 0), do: x
+    def of(x, y), do: of(y, rem(x, y))
+end
