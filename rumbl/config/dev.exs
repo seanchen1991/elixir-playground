506ebe137bf3b1c993c659b45552e62d02e82e5d
Sean Chen <skypemaster007@gmail.com>
2016-01-08 15:40:32 -0800

    Replace in-memory repo with database-backed model

--- rumbl/config/dev.exs
+++ rumbl/config/dev.exs
@@ -36,8 +36,8 @@ config :phoenix, :stacktrace_depth, 20
 # Configure your database
 config :rumbl, Rumbl.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
+  username: "seanchen",
+  password: "seanchen",
   database: "rumbl_dev",
   hostname: "localhost",
   pool_size: 10
