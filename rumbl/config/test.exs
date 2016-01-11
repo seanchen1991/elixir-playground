8c55d2e5627bd40fb74e6552d6e43a37d141dbde
Sean Chen <skypemaster007@gmail.com>
2016-01-11 15:35:56 -0800

    Add unit tests for authentication plugs

--- rumbl/config/test.exs
+++ rumbl/config/test.exs
@@ -9,6 +9,10 @@ config :rumbl, Rumbl.Endpoint,
 # Print only warnings and errors during test
 config :logger, level: :warn
 
+# Configure crypto library to ease up on hashing during testing
+config :comeonin, :bcrypt_log_rounds, 4
+config :comeonin, :pbkdf2_rounds, 1
+
 # Configure your database
 config :rumbl, Rumbl.Repo,
   adapter: Ecto.Adapters.Postgres,
